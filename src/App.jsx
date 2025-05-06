e1659324b23db27e73b55391e9efb1ae6210aecc
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-05-06 18:51:13 +0200

    Refactor App component to improve structure and add introductory content

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 // Import required  hooks
-import { DogFact } from "./components/DogFact";
+import { DogFact } from "./components/DogFact.jsx";
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
   // Hint: Define the API endpoint
   // Hint: Create a function to fetch the dog fact
+  use Effect (() => {
+    [dependency])
+  })
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
 
   return (
     <div className="App">
-      <DogFact />
-    </div>
+       <>
+      <h1>Dogfacts and APIs</h1>
+      <p>Let's practise calling API's within a Dogfact project</p>
+      <P Dogfacts />
+    </>
+  );
+};
+
+export default App;
   );
 };
