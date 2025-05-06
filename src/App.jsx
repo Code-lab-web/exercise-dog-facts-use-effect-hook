eacdfc2f1a283ef6020d48ef7e31304360c3ad4a
Johan Skarpsvärd <145796632+Code-lab-web@users.noreply.github.com>
2025-05-06 14:46:13 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 // Import required  hooks
-import { DogFact } from "./components/DogFact";
+import { DogFact } from "./components/DogFact.jsx";
+import { useEffect} from ".components/DogFact.jsx";
+fech (DogFact) from ".components/DogFact.jsx";
+useEffect hook mounted;
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+useState (Storage) 'numberDog' (1);
   // Hint: Define the API endpoint
   // Hint: Create a function to fetch the dog fact
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
