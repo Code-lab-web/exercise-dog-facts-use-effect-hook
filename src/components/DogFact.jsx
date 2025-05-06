184fbcf1994180e8422177d5559ad22e784ddc7f
Johan Skarpsvärd <145796632+Code-lab-web@users.noreply.github.com>
2025-05-06 12:05:29 +0000

    Main

--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,3 +1,13 @@
+const url = "https://dogapi.dog/api/v2/";
+const fech dogs =async()=>{
+  useEffect (()) =>{
+  }[dependency])
+}dogFact fact={yourFactData} />
+  const Heading =props => {
+    return
+    <h1> { props, heading}</h1>
+  }
+}
 // DogFact Component
 export const DogFact = () => {
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
